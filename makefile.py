3c7d50e5df7d693a1076cb548339f6a82d8092fe
CHarris-VR <clharrisagent@gmail.com>
2025-09-25 20:03:56 -0400

    first commit

---
+++ makefile.py
@@ -0,0 +1,6 @@
+name = input("What's your name? \n")
+favorite_color = input("What is your favorite color? \n")
+
+with open("exmaple.txt", "w") as file:
+    file.write(f"{name} created this file. ")
+    file.write(f"{name}'s favorite is {favorite_color}")
